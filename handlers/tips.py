c9a3c7756cd35e624488d4a5835a41516d34344e
MasterVVK <vvk@2140101.ru>
2024-07-09 18:24:41 +0500

    Update tips.py

--- handlers/tips.py
+++ handlers/tips.py
@@ -1,11 +1,10 @@
 import random
 from aiogram import Router, F
-from aiogram.filters import Command
 from aiogram.types import Message
 
 router = Router()
 
-TIPS = [
+tips = [
     "Совет 1: Ведите бюджет и следите за своими расходами.",
     "Совет 2: Откладывайте часть доходов на сбережения.",
     "Совет 3: Покупайте товары по скидкам и распродажам."
@@ -13,5 +12,5 @@ TIPS = [
 
 @router.message(F.text == "Советы по экономии")
 async def send_tips(message: Message):
-    tip = random.choice(TIPS)
+    tip = random.choice(tips)
     await message.answer(tip)
