24d8f965809fa629610634da7f03ea7e6cc3c1cd
MasterVVK <vvk@2140101.ru>
2024-07-09 21:50:43 +0500

    Update finances.py

--- handlers/finances.py
+++ handlers/finances.py
@@ -6,6 +6,7 @@ from db.database import get_cursor, get_conn
 
 router = Router()
 
+
 class FinancesForm(StatesGroup):
     category1 = State()
     expenses1 = State()
@@ -14,16 +15,29 @@ class FinancesForm(StatesGroup):
     category3 = State()
     expenses3 = State()
 
+
 @router.message(F.text == "Личные финансы")
 async def finances(message: Message, state: FSMContext):
+    telegram_id = message.from_user.id
+    cursor = get_cursor()
+    cursor.execute('''SELECT * FROM users WHERE telegram_id = ?''', (telegram_id,))
+    user = cursor.fetchone()
+
+    if not user:
+        await message.reply(
+            "Вы не зарегистрированы. Пожалуйста, зарегистрируйтесь, отправив сообщение 'Регистрация в телеграм боте'.")
+        return
+
     await state.set_state(FinancesForm.category1)
     await message.reply("Введите первую категорию расходов:")
+
 
 @router.message(FinancesForm.category1)
 async def finances_category1(message: Message, state: FSMContext):
     await state.update_data(category1=message.text)
     await state.set_state(FinancesForm.expenses1)
-    await message.reply("Введите расходы для категории 1:")
+    await message.reply(f"Введите расходы для категории '{message.text}':")
+
 
 @router.message(FinancesForm.expenses1)
 async def finances_expenses1(message: Message, state: FSMContext):
@@ -31,11 +45,13 @@ async def finances_expenses1(message: Message, state: FSMContext):
     await state.set_state(FinancesForm.category2)
     await message.reply("Введите вторую категорию расходов:")
 
+
 @router.message(FinancesForm.category2)
 async def finances_category2(message: Message, state: FSMContext):
     await state.update_data(category2=message.text)
     await state.set_state(FinancesForm.expenses2)
-    await message.reply("Введите расходы для категории 2:")
+    await message.reply(f"Введите расходы для категории '{message.text}':")
+
 
 @router.message(FinancesForm.expenses2)
 async def finances_expenses2(message: Message, state: FSMContext):
@@ -43,11 +59,13 @@ async def finances_expenses2(message: Message, state: FSMContext):
     await state.set_state(FinancesForm.category3)
     await message.reply("Введите третью категорию расходов:")
 
+
 @router.message(FinancesForm.category3)
 async def finances_category3(message: Message, state: FSMContext):
     await state.update_data(category3=message.text)
     await state.set_state(FinancesForm.expenses3)
-    await message.reply("Введите расходы для категории 3:")
+    await message.reply(f"Введите расходы для категории '{message.text}':")
+
 
 @router.message(FinancesForm.expenses3)
 async def finances_expenses3(message: Message, state: FSMContext):
@@ -57,7 +75,9 @@ async def finances_expenses3(message: Message, state: FSMContext):
     conn = get_conn()
     cursor.execute('''
         UPDATE users SET category1 = ?, expenses1 = ?, category2 = ?, expenses2 = ?, category3 = ?, expenses3 = ? WHERE telegram_id = ?
-    ''', (data['category1'], data['expenses1'], data['category2'], data['expenses2'], data['category3'], float(message.text), telegram_id))
+    ''', (
+    data['category1'], data['expenses1'], data['category2'], data['expenses2'], data['category3'], float(message.text),
+    telegram_id))
     conn.commit()
     await state.clear()
     await message.answer("Категории и расходы сохранены!")
