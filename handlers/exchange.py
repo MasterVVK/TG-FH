dba817da9715180c097e2cda041cf142b3ca4d1b
MasterVVK <vvk@2140101.ru>
2024-07-09 23:13:33 +0500

    Update exchange.py

--- handlers/exchange.py
+++ handlers/exchange.py
@@ -22,6 +22,7 @@ async def exchange_rates(message: Message):
                 logging.info(f"Получен ответ от API курса валют: статус {response.status}, URL запроса {response.url}")
                 if response.status == 200:
                     data = await response.json()
+                    logging.info(f"Ответ от exchangerate-ap: {data}")
                     usd_to_rub = data['conversion_rates']['RUB']
                     eur_to_usd = data['conversion_rates']['EUR']
                     euro_to_rub = eur_to_usd * usd_to_rub
